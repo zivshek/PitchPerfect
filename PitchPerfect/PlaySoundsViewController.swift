73a1143780e6209372b5bf8d35f2471168ae886c
zivshek <ziv.hao.shek@gmail.com>
2017-06-21 15:55:52 -0400

    solved stop button

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -12,6 +12,14 @@ class PlaySoundsViewController: UIViewController {
 
     var recordedAudioURL: URL!
     
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
